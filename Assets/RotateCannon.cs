e019989686cdb277eb7513b8d74e640edd80dc3d
Niels Meyering <n.meyering@salt-and-pepper.eu>
2018-06-08 18:51:45 +0200

    Initial import

---
+++ Assets/RotateCannon.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class RotateCannon : MonoBehaviour
+{
+	public CircularDrive azimuth;
+	private Quaternion startRotation;
+
+	void Start ()
+	{
+		startRotation = transform.localRotation;
+	}
+	
+	void Update () {
+		transform.localRotation = 
+			startRotation *
+			Quaternion.AngleAxis(azimuth.outAngle, Vector3.up);
+	}
+}
