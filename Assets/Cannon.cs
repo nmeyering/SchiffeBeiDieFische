2a6188aaa4d1d084622bf0be87c10c4515d17511
Niels Meyering <n.meyering@salt-and-pepper.eu>
2018-06-09 18:04:46 +0200

    Add folders; WIP better cannon rotation

---
+++ Assets/Cannon.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    public float gearRatioAzimuth = 1;
+    public float gearRatioElevation = 1;
+}
--- Assets/RotateCannon.cs
+++ Assets/RotateCannon.cs
@@ -1,21 +1,60 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 using Valve.VR.InteractionSystem;
 
 public class RotateCannon : MonoBehaviour
 {
-	public CircularDrive azimuth;
+	public CircularDrive drive;
+	public Axis rotationAxis;
+	
 	private Quaternion startRotation;
+	private Cannon cannon;
 
 	void Start ()
 	{
 		startRotation = transform.localRotation;
+		cannon = GetComponentInParent<Cannon>();
 	}
 	
 	void Update () {
 		transform.localRotation = 
 			startRotation *
-			Quaternion.AngleAxis(azimuth.outAngle, Vector3.up);
+			Quaternion.AngleAxis(
+				GearRatio(cannon, rotationAxis)
+				* drive.outAngle,
+				AxisVector(rotationAxis));
+	}
+
+	private static float GearRatio(Cannon cannon, Axis axis)
+	{
+		switch (axis)
+		{
+			case Axis.X:
+				return 1 / cannon.gearRatioElevation;
+			case Axis.Y:
+				return 1 / cannon.gearRatioAzimuth;
+			default:
+                return 1;
+		}
+
+	}
+
+	private static Vector3 AxisVector(Axis axis)
+	{
+		switch (axis)
+		{
+			case Axis.X:
+				return Vector3.right;
+			case Axis.Y:
+				return Vector3.up;
+			case Axis.Z:
+				return Vector3.forward;
+			case Axis.None:
+			default:
+				throw new ArgumentOutOfRangeException("axis", axis, null);
+		}
 	}
 }
